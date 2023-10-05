20dfc2c335bce5c3af7bc278b5cf8de6456ade75
egnzz <egnzz@mail.ru>
2023-10-05 22:23:16 +0300

    Исправлено задание N4, добавлена 1-я задача 5-го задания

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,6 +1,9 @@
 export default class ColumnChart {
   chartHeight = 50;
-  //templateLink = this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : "";
+
+  element;
+  subElements = {};
+
   constructor(props = {}) {
     const {
       data,
@@ -14,45 +17,64 @@ export default class ColumnChart {
     this.label = label;
     this.link = link;
     this.value = formatHeading ? formatHeading(value) : value;
-    //this.templateLink = this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : "";
-    this.skeleton = '<image src="charts-skeleton.svg">';
+
     this.render();
   }
 
   render() {
-    this.element = document.createElement("div");
-    this.element.classList.add("column-chart");    
-    this.element.style = `--chart-height: ${this.chartHeight}`;
-    this.element.innerHTML = `${this.createTemplate(this.data)}`;
+    this.element = this.createElement(this.createTemplate(this));
+
+    this.subElements = {
+      body: this.element.querySelector('[data-element="body"]')
+    };
   }
 
-  createTemplate(data) {
+  createElement(template) {
+    const element = document.createElement("div");
+    
+    element.innerHTML = template;
+
+    return element.firstElementChild;
+  }
+
+  createTemplate(props) {
     return `
-    <div class="column-chart__title">${this.label}${this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : ""}</div> 
-    <div class="column-chart__container">
-      <div class="column-chart__header" data-element="header">${this.value}</div>
-      <div class="column-chart__chart ${data ? 'column-chart_loading' : ''}" data-element="body">     
-      ${data ? "" : this.skeleton}
-      ${data ? this.getColumnProps(data) : ""}
-      ${data ? "" : this.element.classList.add("column-chart_loading")}
+      <div class="column-chart ${this.data ? '' : 'column-chart_loading'}" style="--chart-height: ${props.chartHeight}">
+        <div class="column-chart__title">
+          ${props.label}
+          ${props.link ? `<a class="column-chart__link" href="${props.link}">View all</a>` : ""}
+        </div> 
+        <div class="column-chart__container">
+          <div class="column-chart__header" data-element="header">${this.value}</div>
+          <div class="column-chart__chart ${this.data ? 'column-chart_loading' : ''}" data-element="body">
+            ${this.createColumnChartBodyTemplate()}
+          </div>
+        </div>
       </div>
-    </div>
     `;
   }
 
+  createColumnChartBodyTemplate() {
+    return `
+      ${this.data ? '' : '<image src="charts-skeleton.svg">'}
+      ${this.data ? this.createColumnPropsTemplate() : ""}  
+    `;
+  }
 
-  getColumnProps(data) {
-    const maxValue = Math.max(...data);
-    return data.map(item => {
-      return `<div
-               style="--value: ${String(Math.floor(item * this.chartHeight / maxValue))}"
-               data-tooltip = ${(item / maxValue * 100).toFixed(0) + '%'}>
-               </div>`;
+  createColumnPropsTemplate() {
+    const maxValue = Math.max(...this.data);
+    return this.data.map(item => {
+      return `
+        <div
+          style="--value: ${String(Math.floor(item * this.chartHeight / maxValue))}"
+          data-tooltip = ${(item / maxValue * 100).toFixed(0) + '%'}>
+        </div>`;
     }).join("");
   }
 
   update(newData) {
-    this.element.innerHTML = this.createTemplate(newData);
+    this.data = newData;
+    this.subElements.body.innerHTML = this.createColumnChartBodyTemplate();
   }
 
   remove() {
@@ -62,5 +84,4 @@ export default class ColumnChart {
   destroy() {
     this.remove();
   }
-
 }
--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,51 @@
 export default class NotificationMessage {
+    constructor(message,  props = {} ) {
+        const {
+            duration = 2000,
+            type = 'success',
+        } = props;
+
+        this.duration = duration,
+        this.type = type,
+        this.message = message,
+
+    this.element = document.createElement("div");
+    this.element.textContent = this.message;
+    this.element.classList.add(this.type);
+    }
+
+    createTemplateElement(element) {
+    return `
+    <div class="notification ${this.type}" style="--value:20s;">
+     <div class="timer"></div>
+      <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+       ${this.message}
+      </div>
+     </div>
+    </div>`;
+}
+
+    show(elem) {
+    if(elem) this.element = elem;
+	document.body.append(this.element);
+     this.element.innerHTML = this.createTemplateElement(this.element);
+
+	 this.timer = setTimeout(() => {
+	 this.destroy();
+	},
+	this.duration
+	);
+ }
+
+ remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.timer = null;
+    this.remove();
+  }
 
 }
