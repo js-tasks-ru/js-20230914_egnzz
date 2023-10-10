928e7c5c69b4efc35a4a3d38583620b79e3ef858
egnzz <egnzz@mail.ru>
2023-10-10 21:51:00 +0300

    Исправлена задача 5-2, добавлена 6-2 (Tooltip)

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,96 +1,113 @@
 export default class SortableTable {
 
-  subElements = {};
-
   constructor(headerConfig = [], data = []) {
     this.headerConfig = headerConfig;
     this.data = data;
 
     this.element = this.createElement(this.createTemplate());
-
+    
     this.subElements = {
       body: this.element.querySelector('[data-element="body"]')
     };
-
   }
-  
+
   createElement(template) {
-    this.element = document.createElement("div");
-    this.element.innerHTML = `<div data-element="productsContainer" class="products-list__container">${template}</div>`;
-    return this.element.firstElementChild;
+    const element = document.createElement("div");
+    
+    element.innerHTML = template;
+
+    return element.firstElementChild;
   }
 
-  createTemplate() { 
-    const bodyData = this.data.map((item) => {
-      return `
-  <div data-element="body" class="sortable-table__body">
-  <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
-    <div class="sortable-table__cell">
-      <img class="sortable-table-image" alt="Image" src="${'item.images[0].url'}">
-    </div>
-    <div class="sortable-table__cell">${item.title}</div>
-
-    <div class="sortable-table__cell">${item.quantity}</div>
-    <div class="sortable-table__cell">${item.price}</div>
-    <div class="sortable-table__cell">${item.sales}</div>
-  </a>
-</div>
-`;
-    }).join('');
-
-    const headerData = this.headerConfig.map((item) => {
-      item.id.includes('title') ? this.arrow = `
-        <span data-element="arrow" class="sortable-table__sort-arrow">
-        <span class="sort-arrow"></span>
-        </span>`
-        : this.arrow = "";
-
-      return `
-       <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
-        <span>${item.title}</span>
-        ${this.arrow}
-      </div>
-   `;
-    }).join('');
-
+  createTemplate() {
     return `
-  <div class="sortable-table">
-    <div data-element="header" class="sortable-table__header sortable-table__row">
-    ${headerData}
-    </div>
-    ${bodyData}
-    <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
-    <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
-      <div>
-        <p>No products satisfies your filter criteria</p>
-        <button type="button" class="button-primary-outline">Reset all filters</button>
-      </div>
-    </div>
-  </div>
-`;
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.createHeaderTableTemplate()}
+          </div>
+          <div data-element="body" class="sortable-table__body">
+            ${this.createBodyTableTemplate()}
+          </div>
+          <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+          <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+            <div>
+              <p>No products satisfies your filter criteria</p>
+              <button type="button" class="button-primary-outline">Reset all filters</button>
+            </div>
+          </div>
+        </div>
+      </div>`;
   }
 
+  createArrowTemplate() {
+    return `
+        <span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>
+      `;
+  }
+
+  createHeaderTableTemplate() {
+    return this.headerConfig.map((item) => {
+
+      item.id.includes('title')
+        ? this.arrow = this.createArrowTemplate()
+        : this.arrow = "";
+  
+      return `
+         <div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}" 'data-order="asc"'>
+          <span>${item.title}</span>
+          ${this.arrow}
+        </div>
+     `;
+    }).join('');
+  }
+
+  createBodyTableTemplate() {
+    return this.data.map((item) => `
+        <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
+          ${this.createRowTableTemplate(item)}
+        </a>
+      `).join('');
+  }
+
+  createRowTableTemplate(item) {
+    return this.headerConfig.map((headerItem, n) => {
+      return `
+          <div class="sortable-table__cell">${item[this.headerConfig[n].id]}</div>
+	      `;}
+    ).join('');
+  }
+ 
   sort(fieldValue, orderValue) {
     const direction = orderValue === 'asc' ? 1 : -1;
-
-    if (fieldValue === ("title") || ("quantity") || ("price") || ("sales"))
+  
+    if (["title", "quantity", "price", "sales"].includes(fieldValue)) 
     {
       if (fieldValue === "title") {
         this.sortedData = this.data.sort((a, b) => {
           return direction * (a[fieldValue]).localeCompare((b[fieldValue]), ["ru", "en"], { caseFirst: 'upper'});
         });
       }
-
+  
       else {
         this.sortedData = this.data.sort((a, b) => {
           return direction * (a[fieldValue] - b[fieldValue]); 
         });
       }
-
+  
       this.data = this.sortedData;
       this.element.innerHTML = this.createTemplate();
-
+  
     }
+
+    this.update(this.sortedData);
+  }
+
+  update(newData) {
+    this.data = newData;
+    this.subElements.body.innerHTML = this.createBodyTableTemplate();
   }
 
   remove() {
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,58 @@
 class Tooltip {
-  initialize () {
+  static lastInstanse;
+  constructor () {
 
+    this.render();
   }
+	
+
+  render() {
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  createElement(template) {
+    const element = document.createElement("div");
+
+    element.innerHTML = template;
+
+    return element.firstElementChild;
+  }
+	
+  createTemplate() {
+    return `
+    <div class="tooltip">This is tooltip</div>
+    `;
+  }
+
+
+  initialize() {
+
+    if (Tooltip.lastInstanse) {
+      Tooltip.lastInstanse.destroy();
+    }
+
+    Tooltip.lastInstanse = this;
+
+    this.tooltip = document.querySelector('[data-tooltip = "bar-bar-bar"]');
+
+    this.tooltip.addEventListener(("pointerover"), this.handlerOver);
+    this.tooltip.addEventListener(("pointerout"), this.handlerOut); 
+  
+    this.handlerOver = () => { document.body.append(this.element); };   
+    this.handlerOut = () => { this.destroy(); };
+  }
+  
+  
+  remove() {
+    this.element.remove();
+  }
+  
+  destroy() {
+    this.remove();
+    this.tooltip.removeEventListener(("pointerover"), this.handlerOver);
+    this.tooltip.removeEventListener(("pointerout"), this.handlerOut);
+  }
+  
 }
 
 export default Tooltip;
